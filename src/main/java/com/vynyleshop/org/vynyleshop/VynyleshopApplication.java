59e72e0291e66c41d670080af1cf4e8594806f21
SimoneChiodo <simochi.005@gmail.com>
2025-05-13 13:26:07 +0200

    first commit

---
+++ src/main/java/com/vynyleshop/org/vynyleshop/VynyleshopApplication.java
@@ -0,0 +1,13 @@
+package com.vynyleshop.org.vynyleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VynyleshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VynyleshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vynyleshop/org/vynyleshop/VynyleshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vynyleshop.org.vynyleshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VynyleshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
