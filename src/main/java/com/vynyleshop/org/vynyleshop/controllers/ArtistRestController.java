a1af15b8689bdfe77b82e56f7332eadc8c2087b4
SimoneChiodo <simochi.005@gmail.com>
2025-05-16 17:15:02 +0200

    Fix frontend access

--- src/main/java/com/vynyleshop/org/vynyleshop/controllers/ArtistRestController.java
+++ src/main/java/com/vynyleshop/org/vynyleshop/controllers/ArtistRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import com.vynyleshop.org.vynyleshop.model.Artist;
 import com.vynyleshop.org.vynyleshop.service.ArtistService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5000") // Allow access only to React app
 @RequestMapping("/api/artist")
 public class ArtistRestController {
   
